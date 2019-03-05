105f61b92588c180e933d7ec36e7902e3891e205
sandeand <39066304+sandeand@users.noreply.github.com>
2019-03-05 00:16:46 -0800

    Add files via upload

--- projects/sandeand/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ projects/sandeand/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -7,9 +7,6 @@ import junit.framework.TestCase;
 // Again, it is up to you to use this file or not!
 
 
-
-
-
 public class UrlValidatorTest extends TestCase {
 
 
@@ -19,11 +16,59 @@ public class UrlValidatorTest extends TestCase {
 
    
    
-   public void testManualTest()
+   public void testManualTesthttp()
    {
-//You can use this function to implement your manual testing	   
-	   
+      //You can use this function to implement your manual testing	   
+      UrlValidator valid = new UrlValidator();	   
+      assertTrue(valid.isValid("http://www.google.com"));
+      assertFalse(valid.isValid("htttp://www.google.com"));
    }
+
+   public void testManualTestwww()
+   {
+      //You can use this function to implement your manual testing	   
+      UrlValidator valid = new UrlValidator();	   
+      assertTrue(valid.isValid("http://www.google.com"));
+      assertFalse(valid.isValid("http://ww.google.com"));
+   }
+   
+   public void testManualTestcom()
+   {
+      //You can use this function to implement your manual testing	   
+      UrlValidator valid = new UrlValidator();	   
+      assertTrue(valid.isValid("http://www.google.com"));
+      assertFalse(valid.isValid("http://www.google.comm"));
+   }
+   
+   public void testManualTestdot()
+   {
+      //You can use this function to implement your manual testing	   
+      UrlValidator valid = new UrlValidator();	   
+      assertTrue(valid.isValid("http://www.google.com"));
+      assertFalse(valid.isValid("http://www.google..com"));
+   }
+   
+   public void testManualTestpup()
+   {
+      //You can use this function to implement your manual testing	   
+      UrlValidator valid = new UrlValidator();	   
+      assertTrue(valid.isValid("https://www.google.com/search?q=greyhound+puppies&tbm=isch"));
+   }
+   
+   public void testManualTestgov()
+   {
+      //You can use this function to implement your manual testing	   
+      UrlValidator valid = new UrlValidator();	   
+      assertTrue(valid.isValid("https://www.epa.gov/"));
+   }
+   
+   public void testManualTestfile()
+   {
+      //You can use this function to implement your manual testing	   
+      UrlValidator valid = new UrlValidator(new String[] {"http","file"}, UrlValidator.ALLOW_LOCAL_URLS);
+      assertTrue(valid.isValid("file:///C:/user/andrew/eclipse/"));
+   }
+   
    
    
    public void testYourFirstPartition()
